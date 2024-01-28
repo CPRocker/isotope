a5cbdd9aaf60b9f1fcb3170fa0c9d99c3a184157
Carson Price <carson.price@mines.sdsmt.edu>
2024-01-28 13:43:10 -0700

    Create base file input flow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() <= 1 {
+        panic!("Requires cmd line args to run");
+    }
+
+    let file_path = args.get(1)
+        .expect("Requires file to compile");
+
+    if !file_path.ends_with(".isotope") {
+        panic!("File must end with `.isotope`");
+    }
+
+    let contents = fs::read_to_string(file_path)
+        .unwrap_or_else(|_| panic!("Cannot read file: `{file_path}`"));
+
+    dbg!(contents);
 }
