1d6d06425df3dc3f44296a50e62574af8b1d3b90
Carson Price <carson.price@mines.sdsmt.edu>
2024-09-04 16:39:27 -0600

    Test positive lexer routes

--- src/lib/lexer.rs
+++ src/lib/lexer.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use miette::{Diagnostic, SourceSpan};
 use thiserror::Error;
 
@@ -46,13 +48,13 @@ impl std::fmt::Display for LexerError {
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Token<'de> {
-    pub kind: TokenKind,
+    pub kind: TokenKind<'de>,
     pub orig: &'de str,
     pub offset: usize,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum TokenKind {
+pub enum TokenKind<'de> {
     // single character
     Equals,
     LeftParen,
@@ -67,9 +69,30 @@ pub enum TokenKind {
     Let,
     Return,
     // terminals
-    Identifier,
-    String,
+    Identifier(&'de str),
+    String(&'de str),
     Number(f64),
+}
+
+impl Display for TokenKind<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TokenKind::Equals => write!(f, "="),
+            TokenKind::LeftParen => write!(f, "("),
+            TokenKind::RightParen => write!(f, ")"),
+            TokenKind::Minus => write!(f, "-"),
+            TokenKind::Plus => write!(f, "+"),
+            TokenKind::Slash => write!(f, "/"),
+            TokenKind::Star => write!(f, "*"),
+            TokenKind::Dot => write!(f, "."),
+            TokenKind::Semicolon => write!(f, ";"),
+            TokenKind::Let => write!(f, "let"),
+            TokenKind::Return => write!(f, "return"),
+            TokenKind::Identifier(id) => write!(f, "{}", id),
+            TokenKind::String(s) => write!(f, "\"{}\"", s), // TODO: escape strings
+            TokenKind::Number(n) => write!(f, "{}", n),
+        }
+    }
 }
 
 pub struct Lexer<'de> {
@@ -158,7 +181,7 @@ impl<'de> Iterator for Lexer<'de> {
                 self.rest = &c_onwards[id.len()..];
                 self.byte = offset + id.len();
 
-                macro_rules! ident {
+                macro_rules! reserved {
                     ($kind:ident) => {
                         Some(Ok(Token {
                             kind: TokenKind::$kind,
@@ -169,28 +192,34 @@ impl<'de> Iterator for Lexer<'de> {
                 }
 
                 match id {
-                    "let" => ident!(Let),
-                    "return" => ident!(Return),
-                    _ => ident!(Identifier),
+                    "let" => reserved!(Let),
+                    "return" => reserved!(Return),
+                    _ => Some(Ok(Token {
+                        kind: TokenKind::Identifier(id),
+                        orig: id,
+                        offset,
+                    })),
                 }
             }
             Started::Number => {
-                let num = c_onwards
+                let mut num = c_onwards
                     .split_once(|d| !matches!(d, '0'..='9' | '.'))
                     .map_or(c_onwards, |(num, _)| num);
-                let parts: Vec<&str> = num.split('.').collect();
+                let parts: Vec<&str> = dbg!(num.split('.').collect());
 
                 let n = match parts.len() {
                     1 => {
                         // int
-                        let num = parts[0];
+                        let s = parts[0];
+                        num = &c_onwards[..s.len()];
                         num.parse::<u32>()
                             .expect("Must be an integer from match above")
                             .into()
                     }
                     2.. => {
                         // float
-                        let num = parts[0..=1].join(".");
+                        let s = parts[0..=1].join(".");
+                        num = &c_onwards[..s.len()];
                         num.parse::<f64>()
                             .expect("Must be a float from match above")
                     }
@@ -246,7 +275,7 @@ impl<'de> Iterator for Lexer<'de> {
                     self.byte += literal.len() + '"'.len_utf8();
                     let offset = self.byte - literal.len() - 2 * '"'.len_utf8();
                     return Some(Ok(Token {
-                        kind: TokenKind::String,
+                        kind: TokenKind::String(literal),
                         orig: &self.whole[offset..self.byte],
                         offset,
                     }));
@@ -259,4 +288,123 @@ impl<'de> Iterator for Lexer<'de> {
             }
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::TokenKind::*;
+    use super::*;
+
+    macro_rules! assert_token {
+        ($lexer:ident, $kind:ident, $orig:literal, $offset:literal) => {
+            if let Some(Ok(Token {
+                kind: TokenKind::$kind,
+                orig,
+                offset,
+            })) = $lexer.next()
+            {
+                assert_eq!(orig, $orig);
+                assert_eq!(offset, $offset);
+            } else {
+                panic!("Expected a {} token", $kind);
+            }
+        };
+        ($lexer:ident, $kind:ident, $orig:literal, $offset:literal, $value:literal) => {
+            if let Some(Ok(Token {
+                kind: TokenKind::$kind(val),
+                orig,
+                offset,
+            })) = $lexer.next()
+            {
+                assert_eq!(val, $value);
+                assert_eq!(orig, $orig);
+                assert_eq!(offset, $offset);
+            } else {
+                panic!("Expected a {} token", $kind($value));
+            }
+        };
+    }
+
+    #[test]
+    fn single_line_comments() {
+        let src = r#"
+        // this is a single line comment
+        // this is a second line comment
+        ;
+        "#;
+        let mut lexer = Lexer::new(src);
+
+        assert_token!(lexer, Semicolon, ";", 91);
+    }
+
+    #[test]
+    fn block_comments() {
+        let src = r#"
+        /*
+            this is a block comment
+        */
+        ;
+        "#;
+        let mut lexer = Lexer::new(src);
+
+        assert_token!(lexer, Semicolon, ";", 67);
+    }
+
+    #[test]
+    fn numbers() {
+        let src = "123 123.456 123.456.789";
+        let mut lexer = Lexer::new(src);
+
+        assert_token!(lexer, Number, "123", 0, 123.);
+        assert_token!(lexer, Number, "123.456", 4, 123.456);
+        assert_token!(lexer, Number, "123.456", 12, 123.456);
+        assert_token!(lexer, Dot, ".", 19);
+        assert_token!(lexer, Number, "789", 20, 789.);
+    }
+
+    #[test]
+    fn identifiers() {
+        let src = "foo bar baz";
+        let mut lexer = Lexer::new(src);
+
+        assert_token!(lexer, Identifier, "foo", 0, "foo");
+        assert_token!(lexer, Identifier, "bar", 4, "bar");
+        assert_token!(lexer, Identifier, "baz", 8, "baz");
+    }
+
+    #[test]
+    fn strings() {
+        let src = r#""foo" "bar" "b
+        a
+        z""#;
+        let mut lexer = Lexer::new(src);
+
+        assert_token!(lexer, String, "\"foo\"", 0, "foo");
+        assert_token!(lexer, String, "\"bar\"", 6, "bar");
+        assert_token!(
+            lexer,
+            String,
+            "\"b\n        a\n        z\"",
+            12,
+            "b\n        a\n        z"
+        );
+    }
+
+    #[test]
+    fn whitespace() {
+        let src = "  \t\n\r";
+        let mut lexer = Lexer::new(src);
+
+        assert!(lexer.next().is_none());
+    }
+
+    #[test]
+    fn eof() {
+        let src = "";
+        let mut lexer = Lexer::new(src);
+
+        assert!(lexer.next().is_none());
+    }
+
+    // TODO: test errors
 }
